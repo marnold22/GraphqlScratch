28b96bf432eb7de88ea6e781ede2b1ca1c12d20e
Michael Arnold <minimearnold@gmail.com>
2018-03-09 15:31:55 -0800

    Started implementing json server

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -9,11 +10,11 @@ const {
 
 
 // Hardcoded data
-const customers = [
-    {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:35},
-    {id:'2', name:'Steve Smith', email:'steve@gmail.com', age:25},
-    {id:'3', name:'Sara Williams', email:'sara@gmail.com', age:32},
-];
+// const customers = [
+//     {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:35},
+//     {id:'2', name:'Steve Smith', email:'steve@gmail.com', age:25},
+//     {id:'3', name:'Sara Williams', email:'sara@gmail.com', age:32},
+// ];
 
 
 // Customer Type
@@ -37,19 +38,33 @@ const RootQuery= new GraphQLObjectType({
                 id:{type:GraphQLString}
             },
             resolve(parentValue, args){
-                for(let i = 0;i < customers.length;i++){
-                    if(customers[i].id == args.id){
-                        return customers[i];
-                    }
-                }
+                //Search through hardcoded data
+                // for(let i = 0;i < customers.length;i++){
+                //     if(customers[i].id == args.id){
+                //         return customers[i];
+                //     }
+                // }
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                            .then(res => res.data);
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-              return customers;
+              return axios.get('http://localhost:3000/customers/')
+                          .then(res => res.data);
+              // This is for hardcoded data
+              // return customers;
             }
         }
+    }
+});
+
+// Mutations
+const mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields: {
+
     }
 });
 
